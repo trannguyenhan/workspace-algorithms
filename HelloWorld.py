472ad3267539543a906a9ac14d95d8a381b5572c
trannguyenhan <trannguyenhan01092000@gmail.com>
2021-12-09 14:56:08 +0700

    delete recycle

--- HelloWorld.py
+++
@@ -1,8 +0,0 @@
-b = raw_input();
-
-a = "Hello World"
-
-if a == "Hello World":
-	print(a)
-else:
-	print("Not Hello World")
--- helloworldpython.py
+++
@@ -1,5 +0,0 @@
-print("Hello World python")
-str = input("Nhap vao 1 so ban o")
-if str == "hello Huy":
-    print("Xin chao ! ")
-
